f7ffbef47e0bece011b7dc2ce3967db84435e55a
Kelvin Severino <kseveri@outlook.com>
2024-05-23 08:27:10 -0300

    Rotas

--- routes/book.js
+++ routes/book.js
@@ -4,7 +4,23 @@ const router = Router()
 
 //req = request, res = response
 router.get('/', (req, res) => {
-    res.send("Olá mundo!")
+    res.send("Você fez uma requisição do tipo GET")
+})
+
+router.post('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo POST")
+})
+
+router.patch('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PATCH")
+})
+
+router.put('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo PUT")
+})
+
+router.delete('/', (req, res) => {
+    res.send("Você fez uma requisição do tipo DELETE")
 })
 
 module.exports = router
