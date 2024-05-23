7396afe5beb364952b066ec8c5e4ca8d91c731f0
Kelvin Severino <kseveri@outlook.com>
2024-05-23 11:17:17 -0300

    Usando Parameters

--- services/book.js
+++ services/book.js
@@ -4,6 +4,14 @@ function getAllBooks(){
     return JSON.parse(fs.readFileSync("db.json"))
 }
 
+function getBookById(id){
+    const books =  JSON.parse(fs.readFileSync("db.json"))
+
+    const bookFilter = books.filter(book => book.id === id)[0]
+    return bookFilter
+}
+
 module.exports = {
-    getAllBooks
+    getAllBooks,
+    getBookById
 }
